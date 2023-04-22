f698166529b71577230ca7b736778f6c265c4f4f
Deivy Santos <deivysantos32@gmail.com>
2023-04-22 15:58:39 -0300

    Projeto Fusion Modificado

--- Fusion/settings.py
+++ Fusion/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'Fusion.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,6 +85,11 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
+
+"""
+DATABASES = {
+    'default': dj_database_url.config()
 }
 """
 
@@ -134,7 +139,7 @@ MEDIA_ROOT = str (BASE_DIR / 'media')
 
 
 #E-mail teste console
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 """
 #Email produção
